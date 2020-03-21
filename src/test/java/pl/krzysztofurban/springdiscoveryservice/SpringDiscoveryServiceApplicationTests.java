31ae76b598ce2e5de7b1a1ff8a42bf48a1eeb516
Krzysztof Urban <k.urban@g.pl>
2020-03-21 22:13:36 +0100

    Add eureka service implementation

---
+++ src/test/java/pl/krzysztofurban/springdiscoveryservice/SpringDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.krzysztofurban.springdiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
